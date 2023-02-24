eafc748ba5cc931e27beab6748e7e246aab6fb0d
Ankit Kumar <46848486+ankitdevelops@users.noreply.github.com>
2023-02-24 14:20:39 +0530

    feedback UI section 03

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import FeedbackList from "./Components/FeedbackList";
+import FeedbackStats from "./Components/FeedbackStats";
+import Header from "./Components/Header";
+import FeedbackData from "./data/FeedbackData";
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are You sure you want to delete"))
+      setFeedback(feedback.filter((item) => item.id !== id));
+  };
   return (
-    <div>
-      <h1>Hello from App Component</h1>
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+      </div>
+    </>
   );
 }
 
