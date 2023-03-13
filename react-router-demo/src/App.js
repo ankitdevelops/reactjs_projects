eab88a96c07218191974e332781fa01b643da85a
Ankit Kumar <46848486+ankitdevelops@users.noreply.github.com>
2023-03-13 11:12:57 +0530

    index Route

--- react-router-demo/src/App.js
+++ react-router-demo/src/App.js
@@ -16,6 +16,7 @@ function App() {
         <Route path={"about"} element={<About />} />
         <Route path={"order-summary"} element={<OrderSummary />} />
         <Route path="products" element={<Products />}>
+          <Route index element={<FeaturedProduct />} />
           <Route path="new" element={<New />}></Route>
           <Route path="featured" element={<FeaturedProduct />}></Route>
         </Route>
